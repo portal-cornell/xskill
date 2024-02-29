df1f050521c73a74453a14ec7d29079a055f7ff2
Prithwish Dan <dan.prithwish@gmail.com>
2024-02-29 15:57:01 -0500

    Adding video saving functionality

--- scripts/replay_episodes.py
+++ scripts/replay_episodes.py
@@ -24,8 +24,12 @@ def create_dataset(cfg: DictConfig):
         env = KitchenAllV0(use_abs_action=True, use_sphere_agent=True)
     else:
         raise NotImplementedError
+    store_video, video_path = cfg.store_video, cfg.video_path
+    if store_video: 
+        import imageio
 
     env.reset()
+    frames = []
 
     total_episode = kitchen_dataset.replay_buffer.n_episodes
     # assert cfg.end_eps<=total_episode
@@ -39,7 +43,16 @@ def create_dataset(cfg: DictConfig):
             env.robot.reset(env, reset_pos, env.init_qvel[:].copy())
             image_observations = env.render(width=cfg.res, height=cfg.res)
             image_observations = Image.fromarray(image_observations)
+            frames.append(image_observations)
             env.render(custom=False)
+    
+        if store_video:
+            video_filename = f"rollout_{eps_idx}.mp4"
+            video_filepath = os.path.join(video_path, video_filename)
+            # Save the frames as a video using imageio
+            imageio.mimsave(video_filepath, frames, fps=30)
+    
+    env.close()
 
 
 if __name__ == "__main__":
