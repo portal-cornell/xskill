2de3b5b3f43769a4b473f63d2ebc772e65f8c0ca
Huy Nguyen <hcn9@cornell.edu>
2024-03-26 21:56:41 -0400

    Add support for different subgoals in a task - vary speed of subgoals - pauses between subgoals - use a separate config file for generate new renders

--- scripts/generate_new_render.py
+++ scripts/generate_new_render.py
@@ -31,7 +31,8 @@ ACTION_GOALS = {
     "microwave": [np.array([-0.75])],
     "kettle": [np.array([-0.23, 0.75, 1.62, 0.99, 0.0, 0.0, -0.06])],
     "lift kettle": [
-        np.array([-0.26, 0.5, 1.9, 0.99, 0.0, 0.0, -0.06]),
+        np.array([-0.26, 0.3, 1.9, 0.99, 0.0, 0.0, -0.06]),
+        np.array([-0.26, 0.65, 1.8, 0.99, 0.0, 0.0, -0.06]),
         np.array([-0.23, 0.75, 1.62, 0.99, 0.0, 0.0, -0.06]),
     ],
 }
@@ -39,14 +40,15 @@ ACTION_GOALS = {
 KETTLE_INIT = np.array([-0.269, 0.35, 1.62, 0.99, 0.0, 0.0, 0.0])
 
 
-def set_goal(positions, action_item, start_time, time_count):
+def set_goal(positions, action_item, start_time, time_count, pauses):
     goal = ACTION_GOALS[action_item]
     action_index = ACTION_INDICES[action_item]
     for i in range(len(action_index)):
         position_index = action_index[i]
         start = start_time
         for j in range(len(goal)):
-            duration = int(time_count / len(goal))
+            duration = time_count[j]
+            pause = pauses[j]
             goal_position = goal[j][i]
             change = np.linspace(
                 positions[start][position_index], goal_position, num=duration
@@ -54,7 +56,7 @@ def set_goal(positions, action_item, start_time, time_count):
             end_of_action = start + duration
             positions[start:end_of_action, position_index] = change
             positions[end_of_action:, position_index] = goal_position
-            start = end_of_action
+            start = end_of_action + pause
     return positions
 
 
@@ -68,11 +70,11 @@ def create_pos(
         "kettle",
         "slide cabinet",
     ],
-    durations=[40, 20, 20, 30, 40, 45, 20],
-    pause=25,
+    durations=[[40], [20], [20], [30], [40], [45], [20]],
+    pause=[[25], [25], [25], [25], [25], [25], [25]],
 ):
     assert len(actions) == len(durations)
-    eps_len = sum(durations) + len(durations) * pause
+    eps_len = np.sum(durations) + np.sum(pause)
     res = np.array([[0.0] * 30 for i in range(eps_len)], dtype="f")
     res[:, 23] = np.array([KETTLE_INIT[0]] * eps_len, dtype="f")
     res[:, 24] = np.array([KETTLE_INIT[1]] * eps_len, dtype="f")
@@ -80,13 +82,23 @@ def create_pos(
     res[:, 26] = np.array([KETTLE_INIT[3]] * eps_len, dtype="f")
     start_time = 0
     for task_index in range(len(actions)):
-        res = set_goal(res, actions[task_index], start_time, durations[task_index])
-        start_time = start_time + durations[task_index] + pause
+        res = set_goal(
+            res,
+            actions[task_index],
+            start_time,
+            durations[task_index],
+            pause[task_index],
+        )
+        start_time = (
+            start_time + np.sum(durations[task_index]) + np.sum(pause[task_index])
+        )
     return res
 
 
 @hydra.main(
-    version_base=None, config_path="../config/simulation", config_name="replay_kitchen"
+    version_base=None,
+    config_path="../config/simulation",
+    config_name="generate_kitchen",
 )
 def create_dataset(cfg: DictConfig):
     env = KitchenAllV0(use_abs_action=True, use_sphere_agent=False, use_none=True)
@@ -97,7 +109,7 @@ def create_dataset(cfg: DictConfig):
     env.reset()
     frames = []
 
-    reset_pos = create_pos(["lift kettle", "slide cabinet"], [50, 50], 10)
+    reset_pos = create_pos(["lift kettle"], [[30, 22, 15]], [[10, 0, 0]])
     for i in range(len(reset_pos)):
         env.robot.reset(env, reset_pos[i], env.init_qvel[:].copy())
         image_observations = env.render(width=cfg.res, height=cfg.res)
