b7562127c2d35a1892c1aa62a4e9c448ec4c6d1f
Huy Nguyen <hcn9@cornell.edu>
2024-04-08 22:26:58 -0400

    Remove unused python script

--- scripts/load_data.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-if __name__ == "__main__":
-    actions_seq = np.load("../datasets/kitchen/actions_seq.npy")
-    observations_seq = np.load("../datasets/kitchen/observations_seq.npy")
-    all_observations = np.load("../datasets/kitchen/all_observations.npy")
-    all_init_pos = np.load("../datasets/kitchen/all_init_qpos.npy")
-    all_actions = np.load("../datasets/kitchen/all_actions.npy")
