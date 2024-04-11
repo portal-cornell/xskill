63e4b4871c741ea4403483ad4eed11245919eb7e
Simon Kapen <sak299@en-cc-g2-compute-15.coecis.cornell.edu>
2024-04-11 12:23:50 -0400

    timeline visualization

--- scripts/compute_nearest_neighbors.py
+++ scripts/compute_nearest_neighbors.py
@@ -174,7 +174,7 @@ def label_dataset(cfg: DictConfig):
     config_path="../config/simulation",
     config_name="label_sim_kitchen_dataset",
 )
-def visualize_nn(cfg: DictConfig):
+def visualize_nn(cfg: DictConfig, frame_size=266, skip=4, num_rows=4):
     model = load_model(cfg)
 
     normalize = transforms.Normalize(
@@ -182,79 +182,55 @@ def visualize_nn(cfg: DictConfig):
     )
     pipeline = nn.Sequential(Tr.CenterCrop((112, 112)), normalize)
 
-    clip_list = [247]
-    frame_nums = [180]
+    clip_list = [20, 150, 247]
+    frame_nums = [0, 40, 180]
 
     tasks_list = json.load(open(f'{cfg.data_path}/task_completions.json'))
     clip_diff = 0
     for clip_num in clip_list:
+        human_video_imgs = load_gif(cfg, 'human', clip_num)
+        human_video_imgs = [human_video_imgs[i] for i in range(len(human_video_imgs)) if i % skip == 0]
         for frame_num in frame_nums:
             output_dir = f'similarity_outputs{"_diff" if clip_diff != 0 else ""}/{clip_num}_{frame_num}'
             for file in os.listdir(output_dir):
                 os.remove(os.path.join(output_dir, file))
-            
             os.makedirs(output_dir, exist_ok=True)
+
+            # get distance from each frame in the human clip to the given robot clip frame
             human_clip_num = clip_num - clip_diff
             robot_clip_traj, robot_z = traj_representations(cfg, model, pipeline, 'robot', clip_num, frame_list=[frame_num])
             human_clip_traj, human_z = traj_representations(cfg, model, pipeline, 'human', human_clip_num)
             dists = torch.norm(human_clip_traj - robot_clip_traj, dim=1)
             dists = dists.cpu().detach().numpy()
-            num_top_dists = 3
-            sorted_dists = np.argsort(dists)[:num_top_dists]
-            sorted_dists = np.sort(sorted_dists)
+            closest_dist = np.min(dists)
+            normalized_dists = closest_dist / dists
 
-            gif_of_clip(cfg, 'robot', clip_num, frame_num, 8, output_dir)
-            robot_gif = Image.open(os.path.join(output_dir, f'robot_{clip_num}_index{frame_num}.gif'))
-            # robot_gif = robot_gif.resize(((int)(robot_gif.size[0]/2), (int)(robot_gif.size[1]/2)))
-            bars = plt.bar(range(len(dists)), dists, color='skyblue')
-            for i in range(len(sorted_dists)):
-                bars[sorted_dists[i]].set_color('orange')
-            plt.axis('off')
-            buf = io.BytesIO()
-            buf.seek(0)
-            buf.truncate(0)
-            plt.savefig(buf, format='png', bbox_inches='tight', pad_inches=0)
-            buf.seek(0)
-            img = Image.open(buf)
-            img.paste(robot_gif, ((int)(img.size[0]/2), (int)(img.size[1]/8)))
+            indicator_h = frame_size // 10
+            row_h = frame_size + indicator_h
 
-            # # load images from human video
-            # human_video_imgs = load_gif(cfg, 'human', clip_num)
-            # final_gif = []
-            # # edit matplot graph to highlight corresponding bar
-            # for i in range(len(human_video_imgs)):
-            #     print(i)
-            #     bars = plt.bar(len(dists), dists, color='skyblue')
-            #     bars[i].set_color('orange')
-            #     plt.axis('off')
-            #     buf = io.BytesIO()
-            #     plt.savefig(buf, format='png', bbox_inches='tight', pad_inches=0)
-            #     buf.seek(0)
-            #     cur_frame = Image.open(buf)
-            #     cur_frame.paste(robot_gif, ((int)(cur_frame.size[0]/2), (int)(cur_frame.size[1]/8)))
-            #     # cur_frame.paste(human_video_imgs[i], ((int)(cur_frame.size[0]/4), (int)(cur_frame.size[1]/8)))
-            #     final_gif.append(cur_frame)
-
-                
-            # # add to list of images in GIF
+            ims_per_row = len(human_video_imgs) // num_rows
+            image_width = frame_size * ims_per_row
+            image_height = row_h * (num_rows + 1)
 
             
+            image = Image.new("RGBA", (image_width, image_height))
+            draw = ImageDraw.Draw(image)
+            for i in range(len(human_video_imgs)):
+                if i * skip >= len(dists):
+                    continue
+                row_num = i // ims_per_row
+                col_num = i % ims_per_row
 
-            draw = ImageDraw.Draw(img)
-            for i in range(num_top_dists):
-                clip_idx = sorted_dists[i]
-                gif_of_clip(cfg, 'human', human_clip_num, clip_idx, 8, output_dir)
-                human_gif = Image.open(os.path.join(output_dir, f'human_{clip_num}_index{clip_idx}.gif'))
-                human_gif = human_gif.resize(((int)(img.size[0]/5), (int)(img.size[1]/5)))
+                img = human_video_imgs[i]
+                pos_x = frame_size * col_num
+                pos_y = row_num * row_h
+                image.paste(human_video_imgs[i].resize((frame_size, frame_size)), (pos_x, pos_y))
+                opacity = (int)(255 * (normalized_dists[i * skip]))
+                draw.rectangle([pos_x, pos_y + frame_size, pos_x + frame_size, pos_y + row_h], fill=(66,135,245,opacity))
 
-                bar_x = (int)(img.size[0] / 2 + 2 * (clip_idx - len(dists) / 2))
-                bar_y = (int)(img.size[1] * 8/9)
-                human_gif_x = (int)(img.size[0]/4 + img.size[0]/2 * (i / num_top_dists))
-                human_gif_y = (int)(img.size[1] * 3/4)
-                img.paste(human_gif, (human_gif_x, human_gif_y))
-                # draw.line([(human_gif_x + human_gif.size[0]/2, human_gif_y), (bar_x, bar_y)], fill="black", width=2)
-            # img[0].save(os.path.join(output_dir, "plot.gif"), format='GIF', save_all=True, append_images=final_gif[1:], loop=0)
-            img.save(os.path.join(output_dir, "plot.gif"))
+            robot_gif = gif_of_clip(cfg, 'robot', clip_num, frame_num, 8, output_dir)
+            image.paste(robot_gif[0].resize((frame_size, frame_size)), (image.size[0]//2 - robot_gif[0].size[0]//2, row_h * num_rows + indicator_h))
+            image.save(os.path.join(output_dir, "timeline.png"))
 
 if __name__ == "__main__":
     visualize_nn()
