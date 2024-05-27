Correct code for storing new datasets + change in OT computation

scripts/generate_twohands_dataset.py
@@ -9,6 +9,7 @@ import numpy as np
 from collections import defaultdict
 from actions import *
 # base_dev_dir = "/share/portal/pd337"
+import json
 from xskill.utility.utils import read_json
 
 ACTION_INDICES = {
@@ -148,7 +149,9 @@ def randomize_handgoals():
 KETTLE_INIT = np.array([-0.269, 0.35, 1.62, 0.99, 0.0, 0.0, 0.0])
 HAND_POS = np.array([-0.48, 0.10, 2.05, -0.38, 0.10, 2.05])
 LEFTHAND_POS = np.array([-0.48, 0.10, 2.05])
+INIT_LEFTHAND_POS = np.array([-0.48, 0.10, 2.05])
 RIGHTHAND_POS = np.array([-0.38, 0.10, 2.05])
+INIT_RIGHTHAND_POS = np.array([-0.38, 0.10, 2.055])
 
 
 def interpolate(start, end, ease_function, duration):
@@ -301,14 +304,20 @@ def create_pos(
         left_action = action_groups[group_number][0]["action"]
         left_goal = HAND_GOALS[left_action][0]
         lefthand_righthand_assign_dist += np.linalg.norm(LEFTHAND_POS - left_goal)
-        right_action = action_groups[group_number][1]["action"]
-        right_goal = HAND_GOALS[right_action][0]
-        lefthand_righthand_assign_dist += np.linalg.norm(RIGHTHAND_POS - right_goal)
-        
+        if len(action_groups[group_number]) == 2:
+            right_action = action_groups[group_number][1]["action"]
+            right_goal = HAND_GOALS[right_action][0]
+            lefthand_righthand_assign_dist += np.linalg.norm(RIGHTHAND_POS - right_goal)
+        else:
+            lefthand_righthand_assign_dist += 0
+
         righthand_left_assign_dist = 0
-        left_action = action_groups[group_number][1]["action"]
-        left_goal = HAND_GOALS[left_action][0]
-        righthand_left_assign_dist += np.linalg.norm(LEFTHAND_POS - left_goal)
+        if len(action_groups[group_number]) == 2:
+            left_action = action_groups[group_number][1]["action"]
+            left_goal = HAND_GOALS[left_action][0]
+            righthand_left_assign_dist += np.linalg.norm(LEFTHAND_POS - left_goal)
+        else:
+            righthand_left_assign_dist += 0
         right_action = action_groups[group_number][0]["action"]
         right_goal = HAND_GOALS[right_action][0]
         righthand_left_assign_dist += np.linalg.norm(RIGHTHAND_POS - right_goal)
@@ -341,6 +350,11 @@ def create_pos(
                 open_completion,
                 ease,
             )
+        if len(action_groups[group_number]) == 1:
+            if left_first:
+                handpos[start_time:start_time + durations[group_number], 3:] = RIGHTHAND_POS
+            else:
+                handpos[start_time:start_time + durations[group_number], :3] = LEFTHAND_POS
         start_time = start_time + durations[group_number]
     # breakpoint()
     return res, handpos
@@ -418,10 +432,13 @@ def create_pos(
     config_name="generate_kitchen",
 )
 def generate_render(cfg: DictConfig):
+    global LEFTHAND_POS, RIGHTHAND_POS, INIT_LEFTHAND_POS, INIT_RIGHTHAND_POS
     task_completions_list = read_json(f"/share/portal/kk837/xskill/datasets/kitchen_dataset/task_completions.json")
     # breakpoint()
     for eps_idx, task_list in enumerate(task_completions_list):
         randomize_handgoals()
+        LEFTHAND_POS = INIT_LEFTHAND_POS
+        RIGHTHAND_POS = INIT_RIGHTHAND_POS
         actions = []
         for task in task_list:
             if task == "kettle":
@@ -453,7 +470,12 @@ def generate_render(cfg: DictConfig):
         env.reset()
         frames = []
         # breakpoint()
-        reset_pos, handpos = create_pos(actions, [1, 1, 3, 3])
+        # actions.pop()
+        # breakpoint()
+        if len(actions) == 3:
+            reset_pos, handpos = create_pos(actions, [1, 1, 3])
+        else:
+            reset_pos, handpos = create_pos(actions, [1, 1, 3, 3])
 
         for i in range(len(reset_pos)):
             env.sim.model.body_pos[-2][:3] = handpos[i][:3]
@@ -465,6 +487,10 @@ def generate_render(cfg: DictConfig):
             os.makedirs(os.path.join(cfg.video_path, f'{eps_idx}'), exist_ok=True)
             image_observations.save(video_filepath)
         env.close()
+
+        eps_path = os.path.join(cfg.video_path, f'{eps_idx}')
+        with open(os.path.join(eps_path, "states.json"), "w") as f:
+            json.dump(reset_pos.tolist(), f)
 
         # for i in range(len(reset_pos)):
         # # for i in range(100):

xskill/model/core.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import wandb
 from torch import nn
-
+import time
 
 class Model(pl.LightningModule):
 
@@ -199,14 +199,23 @@ class Model(pl.LightningModule):
         norms = torch.bmm(x_n, y_n.transpose(1, 2))
         C = (1 - C / norms)
         return C
-
+    
     def compute_optimal_transport_loss(self, zc_r, zc_h):
         dist = self.batch_cosine_distance(zc_r, zc_h)
-
         total_loss = 0
+        ct = time.time()
         for i in range(dist.shape[0]):
-            assignment = self.distributed_sinkhorn(dist[i])
-            total_loss += torch.sum(assignment * dist[i])/dist.shape[1]
+            neg_dists = torch.zeros(dist.shape[0])
+            for j in range(dist.shape[0]):
+                cosin_dists = self.batch_cosine_distance(zc_r[i].unsqueeze(0), zc_h[j].unsqueeze(0))
+                assignment = self.distributed_sinkhorn(cosin_dists[0])
+                distance = torch.sum(assignment * cosin_dists[0])
+                neg_dists[j] = torch.exp(-distance)
+                if i == j:
+                    pos_dist = torch.exp(-distance)
+            total_loss += pos_dist/torch.sum(neg_dists)
+        print("Time taken for OT calculcation = ", time.time() - ct)
+        # breakpoint()
         return total_loss
         
     def paired_training_step(self):
@@ -480,3 +489,28 @@ class Model(pl.LightningModule):
 
         Q *= B  # the colomns must sum to 1 so that Q is an assignment
         return Q.t()
+    
+    # @profile
+    # @torch.no_grad()
+    # def batched_distributed_sinkhorn(self, out):
+    #     Q = torch.exp(out / self.epsilon).t(
+    #     )  # Q is K-by-B for consistency with notations from our paper
+    #     B = Q.shape[1]  # number of samples to assign
+    #     K = Q.shape[0]  # how many prototypes
+
+    #     # make the matrix sums to 1
+    #     sum_Q = torch.sum(Q)
+    #     Q /= sum_Q
+
+    #     for it in range(self.sinkhorn_iterations):
+    #         # normalize each row: total weight per prototype must be 1/K
+    #         sum_of_rows = torch.sum(Q, dim=1, keepdim=True)
+    #         Q /= sum_of_rows
+    #         Q /= K
+
+    #         # normalize each column: total weight per sample must be 1/B
+    #         Q /= torch.sum(Q, dim=0, keepdim=True)
+    #         Q /= B
+
+    #     Q *= B  # the colomns must sum to 1 so that Q is an assignment
+    #     return Q.t()